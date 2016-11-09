04c8b1591bc85f1ab5d50dee35a33a4fe3ce0a2e
dorogo <ill.shir@gmail.com>
2016-11-10 02:05:11 +0300

    добавлена переменная'isbuffered' true - режим с буферами - целостность 100% false - режим без буферов - целостность > 80%

--- src/quarantinelab/QuarantineLab.java
+++ src/quarantinelab/QuarantineLab.java
@@ -6,12 +6,9 @@
 package quarantinelab;
 
 import java.io.IOException;
-import java.security.SecureRandom;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
-import java.util.Stack;
-import java.util.Timer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,34 +20,33 @@ public class QuarantineLab {
 
     private static volatile int i = 0;
     private static volatile int le = 0;
-    private static String pathSrcText = System.getProperty("user.dir") + "/packet.txt";
-    private static String pathCleanText = System.getProperty("user.dir") + "/packet2.txt";
+    private static String pathSrcText = System.getProperty("user.dir") + "/srcDirtyText.txt";
+    private static String pathCleanSrcText = System.getProperty("user.dir") + "/srcCleanText.txt";
+    private static String pathCleanText = System.getProperty("user.dir") + "/resultCleanText.txt";
 
-    private static volatile char c;
     private static volatile Character ch;
     private static volatile Queue<Character> fifo = new LinkedList<Character>();
-//    private static volatile Queue<Integer> fifo = new LinkedList<Integer>();
     private static volatile boolean isLive = true;
     private static final boolean test = true;
-//    private static Stack fifo = new Stack();
 
     private static final Object monitor = new Object();
+    
+    
+    private static boolean isBuffered = true;
 
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         Thread myThread1 = new Thread(new Runnable() {
-            String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            
+            String s = "";
 
             public void run() //Этот метод будет выполняться в побочном потоке
             {
                 try {
-                    s = FileHelper.getString(pathCleanText);
+                    s = FileHelper.getString(pathSrcText);
                 } catch (IOException ex) {
                     Logger.getLogger(QuarantineLab.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 le = s.length();
-//                System.out.println("" + Thread.currentThread());
                 synchronized (monitor) {
                     monitor.notify();
                 }
@@ -65,14 +61,14 @@ public class QuarantineLab {
                 while (isLive) {
                     if (i < s.length()) {
                         try {
-                            s = FileHelper.getString(pathCleanText);
+                            s = FileHelper.getString(pathSrcText);
                         } catch (IOException ex) {
                             Logger.getLogger(QuarantineLab.class.getName()).log(Level.SEVERE, null, ex);
                         }
                         ch = new Character(s.charAt(i));
-//                        System.out.println("1" + Thread.currentThread());
                         synchronized (monitor) {
-                            fifo.add(ch);                            
+                            if (isBuffered) //для режима с буфером
+                                fifo.add(ch);
                         }
                     }
                     i++;
@@ -100,39 +96,44 @@ public class QuarantineLab {
                 synchronized (monitor) {
                     monitor.notify();
                 }
-                System.out.println("2- " + System.currentTimeMillis());
-                
+
                 while (isLive) {
-                    System.out.println(""+fifo.size());
                     try {
-                        s = FileHelper.getString(pathCleanText);
+                        s = FileHelper.getString(pathSrcText);
                     } catch (IOException ex) {
                         Logger.getLogger(QuarantineLab.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    
+
                     tmp = ch;
-//                    System.out.println("" + tmp + i);
-                    synchronized(monitor) {
-                            if (fifo.size() > 0) {
-                                result += fifo.poll().toString();
+                    //принимаем символы из буфера и отсеиваем ненужные символы
+                    synchronized (monitor) {
+                        if (isBuffered && fifo.size() > 0) {
+                            if (fifo.peek().toString().matches("[A-Za-z0-9 ]")) {
+                                if (result == null) {
+                                    result = fifo.poll().toString();
+                                } else {
+                                    result += fifo.poll().toString();
+                                }
+                            } else {
+                                fifo.poll();
                             }
                         }
-                    if (i < le && tmp != null) {
-                        
-//                        result += tmp.toString();
-                    } else if (i == le) {
-                        FileHelper.writeString(result, (System.getProperty("user.dir") + "/packet212312.txt"), false);
-                    } else if (i > le && fifo.size() == 0) {
-                        System.out.println("" + fifo.size() + " " + s.length());
-                        System.out.println("res=" + result.length() + " src=" + le + " " + result);
-                        System.out.println("" + ((float) result.length() / (float) le));
+                    }
+                    if (!isBuffered && i < le && tmp != null) {
+                        if (tmp.toString().matches("[A-Za-z0-9 ]"))
+                            result += tmp.toString();
+                    } else if (i > le && ( fifo.size() == 0 || !isBuffered)) {
+                        //запись чистого текста и проверкуа на потери
+                        FileHelper.writeString(result, pathCleanText, false);
+//                        System.out.println("" + fifo.size() + " " + s.length());
                         try {
-                            Thread.sleep(20000);
-                        } catch (InterruptedException ex) {
+                            System.out.println("res=" + result.length() + " src=" + FileHelper.getString(pathCleanSrcText).length() + " " + result);
+                            System.out.println("" + ((float) result.length() / (float)FileHelper.getString(pathCleanSrcText).length() * 100) + "%");
+                        } catch (IOException ex) {
                             Logger.getLogger(QuarantineLab.class.getName()).log(Level.SEVERE, null, ex);
                         }
+                        isLive = false;
                     }
-//                    System.out.println("2" + Thread.currentThread());
                     Thread.yield();
                 }
             }
@@ -140,7 +141,8 @@ public class QuarantineLab {
 
         myThread2.start();	//Запуск потока
         myThread1.start();	//Запуск потока
-//        }
+        
+        //для формирования исходного файла
 //        System.out.println("olololool");
 //        FileHelper.writeString(generateString(new SecureRandom(), "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789       !@#$%^&*_-+=", 7008), (System.getProperty("user.dir") + "/packet.txt"), false);
 //        String tmp = FileHelper.getString( (System.getProperty("user.dir") + "/packet.txt"));
